32b5494346b289e2087247df196507799b530bd0
Rajmani Arya <rajmani1995@gmail.com>
2016-03-20 12:07:08 +0530

    improved ! One threaded Service

--- unix_sockets/TSS/service3.c
+++ unix_sockets/TSS/service3.c
@@ -1,26 +1,57 @@
 #include "utils.h"
+#include <sys/time.h>
 #define BUFSIZE 1024
 /**
- * Service for LOWERCASE Server
+ * Service for UPPERCASE Server
  */
+
+int nsfds[32], nClient=0; 
+pthread_mutex_t lock;
+
 void * runner(void * args) {
-	int i,fd = *(int *)args; char buf[BUFSIZE];
-	printf("start servicing\n");
+	int max,MAXC,i,j; char buf[BUFSIZE];
+	printf("start servicing\n"); struct timeval tv;
+	fd_set rfds;
 	while(1) {
-		read(fd, buf, BUFSIZE);
-		// printf("%s\n", buf);
-		if(atoi(buf) == -1){
-			close(fd);
-			printf("Client Exitting..\n");
-			pthread_exit(0);
+		FD_ZERO(&rfds);
+		pthread_mutex_lock(&lock);
+		max = -1;
+		for(j=0; j<nClient; j++) {
+			if(nsfds[j] == -1) continue;
+			FD_SET(nsfds[j], &rfds);
+			if(max < nsfds[j])
+				max = nsfds[j];
 		}
-		write(fd, buf, strlen(buf));
-		memset(buf, 0, BUFSIZE);
+		MAXC = nClient;
+		pthread_mutex_unlock(&lock);
+		tv.tv_sec = 1; tv.tv_usec = 0;
+		int res = select(max+1, &rfds, NULL, NULL, &tv);
+		if(res < 0) {
+			printf("select() ");
+			continue;
+		}
+		for(i=0; i<MAXC; i++) {
+			if(nsfds[i] == -1) continue;
+			if(FD_ISSET(nsfds[i], &rfds)){
+				read(nsfds[i], buf, BUFSIZE);
+				printf("%s\n", buf);
+				if(atoi(buf) == -1){
+					close(nsfds[i]);
+					nsfds[i] = -1;
+					printf("Client Exitting..\n");
+					continue;
+				}
+
+				write(nsfds[i], buf, strlen(buf));
+				memset(buf, 0, BUFSIZE);
+			}
+		}
 	}
 }
 
 int main(int argc, char const *argv[])
 {
+	pthread_mutex_init(&lock, NULL);
 	if(argc < 2) {
 		printf("Usage: ./service <temperory_sock_path>\n");
 		exit(1);
@@ -30,16 +61,20 @@ int main(int argc, char const *argv[])
 		printf("sock() error\n");
 		exit(1);
 	}
-	printf("service started !!\n");
+	printf("service 3 started !!\n");
 	int fd,n; pthread_t pid;
 	fd_set rfds;
+	pthread_create(&pid, NULL, &runner, NULL);
 	while(1) {
 		fd = recv_fd(usfd);
 		if(fd < 0) {
 			perror("recv_fd() ");
 			continue;
 		}
-		pthread_create(&pid, NULL, runner, (void *) &fd);
+		pthread_mutex_lock(&lock);
+		nsfds[nClient++] = fd;
+		pthread_mutex_unlock(&lock);
+		printf("Client added !\n");
 	}
 	return 0;
 }
